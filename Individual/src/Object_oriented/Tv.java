36eb9a9f6cd824cb7119cc80baea34195209c477
최신형 <tlsgudrhfem@naver.com>
2022-04-05 17:41:53 +0900

    개인

--- Individual/src/Object_oriented/Tv.java
+++ Individual/src/Object_oriented/Tv.java
@@ -1,18 +1,18 @@
 package Object_oriented;
 
-public class Main {
-
-	public static void main(String[] args) {
-		/*
-		 * tv객체를 생성하여 채널을 변경하는 메서드를 실행 할 것
-		 */
-		
-		Tv t = new Tv(); // 티비 객체 생성
-		t.channel = 7;
-		t.channelDown();
-		System.out.println(t.channel);
-	}
-}
+//public class Main {
+//
+//	public static void main(String[] args) {
+//		/*
+//		 * tv객체를 생성하여 채널을 변경하는 메서드를 실행 할 것
+//		 */
+//		
+//		Tv t = new Tv(); // 티비 객체 생성
+//		t.channel = 7;
+//		t.channelDown();
+//		System.out.println(t.channel);
+//	}
+//}
 
 public class Tv {
 	
