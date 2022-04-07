f1f5227ccf31b1dc3d36c370f325721a137f22bd
최신형 <tlsgudrhfem@naver.com>
2022-04-07 20:29:21 +0900

    스탑워치 프로그램 구현중

---
+++ Individual/src/Class_02/Main.java
@@ -0,0 +1,11 @@
+package Class_02;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		StopWatch s = new StopWatch();
+
+	}
+
+}
---
+++ Individual/src/Class_02/StopWatch.java
@@ -0,0 +1,33 @@
+package Class_02;
+
+public class StopWatch {
+	
+	private int hour;
+	private int minute;
+	private int second;
+	
+	public void setHour(int hour) { //setter - 시간을 설정(셋팅)
+		this.hour = hour; // 멤버변수 hour은 매개변수 hour이다.
+	}
+	
+	public int getHour() { //getter - 시간을 가져옴
+		return hour; // 멤버변수 hour을 반환한다. 다시 돌아간다
+	}
+	
+	public void start() {
+
+	}
+	
+	public void stop() {
+		
+	}
+	
+	public void pause() {
+		
+	}
+	
+	public void reset() {
+		
+	}
+
+}
