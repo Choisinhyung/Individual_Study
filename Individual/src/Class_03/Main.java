b96cddaf3632a148aacbc9aeafe71d345d64d4d5
최신형 <tlsgudrhfem@naver.com>
2022-04-07 20:29:33 +0900

    도서관 프로그램 구현중(메서드 제외)

---
+++ Individual/src/Class_03/Main.java
@@ -0,0 +1,16 @@
+package Class_03;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Book book1 = new Book(); //기본생성자
+		Book book2 = new Book("Java Programming");
+		Book book3 = new Book("Java", 10354651);
+		Book book4 = new Book("Programming", 10354651, "미상");
+		Book book5 = new Book("Class Book", 123456777, "최신형", 5);
+
+	}
+
+}
