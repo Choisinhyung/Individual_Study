2dc320bf051d8112472a717e73239b92fdcc9613
최신형 <tlsgudrhfem@naver.com>
2022-04-11 18:19:30 +0900

    책장 넘기는 예제

---
+++ Individual/src/Class_07/Main.java
@@ -0,0 +1,22 @@
+package Class_07;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Page p = new Page(100);
+		
+		for(int i = 0; i < 10; i++) {
+			p.nextPage();
+			System.out.println(p.getPageNumber() + "번째 페이지");			
+		}
+		
+		for(int i = 0; i < 10; i++) {
+			p.prevPage();
+			System.out.println(p.getPageNumber() + "번째 페이지");			
+		}
+
+	}
+
+}
