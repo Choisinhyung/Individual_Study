ad4fb1f4e4296b535cdfeca2eef05063c0bb880c
최신형 <tlsgudrhfem@naver.com>
2022-04-15 16:12:16 +0900

    예제 풀이

--- Individual/src/Class_16/Sample03.java
+++ Individual/src/Class_16/Sample03.java
@@ -1,8 +1,11 @@
 package Class_16;
 
+import java.util.Arrays;
+import java.util.Calendar;
 import java.util.Date;
 import java.text.SimpleDateFormat;
 import java.util.Scanner;
+import java.text.ParseException;
 
 public class Sample03 {
 	
@@ -15,13 +18,28 @@ public class Sample03 {
 		 * (입력 형식은 반드시 년/월/일 형식이어야 하며, 형식이 다른 경우
 		 *  다시 입력하도록 한다.)
 		 */
-		System.out.println("날짜를 입력하세요 : ");
-		String input = sc.nextLine();
 		
-
+		SimpleDateFormat format = new SimpleDateFormat("yyyy/MM/dd");
+		String input = "";
+		Date[] dates = new Date[0];
 		
-		
-		
+		while(true) {
+			System.out.println("날짜를 입력하세요 : ");
+			input = sc.nextLine();
+			
+			if(input.matches("\\d{4}/\\d{1,2}/\\d{1,2}")) {
+				try {
+					dates = Arrays.copyOf(dates, dates.length + 1);
+					dates[dates.length - 1] = format.parse(input);
+				} catch (ParseException e) {
+					e.printStackTrace();
+				}
+			} else if( input.equals("e")) {
+				System.out.println("프로그램을 종료합니다");
+				break;
+			}
+		}
+		System.out.println(Arrays.toString(dates));
 	}
 	
 	
@@ -32,7 +50,51 @@ public class Sample03 {
 		 * 그 차를 구하는 코드를 작성한다.
 		 * (과거의 날짜는 D-Day -10일, 미래의 날짜는 D-Day 10일)
 		 */
+		SimpleDateFormat format = new SimpleDateFormat("yyyy/MM/dd");
+		String input = "";
+		Date[] dates = new Date[1];
 		
+		while(true) {
+			System.out.print("년/월/일 형식의 날짜 입력(e를 입력하면 종료) : ");
+			input = sc.nextLine();
+			if(input.matches("\\d{4}/\\d{1,2}/\\d{1,2}")) {
+				try {
+					dates[0] = format.parse(input);
+				} catch (ParseException e) {
+					e.printStackTrace();
+				}
+				break;
+			}
+		}
+		
+		Calendar c1 = Calendar.getInstance();
+		Calendar c2 = Calendar.getInstance();
+		c1.setTime(dates[0]);
+		try {
+			c2.setTime(format.parse(format.format(new Date())));	// 현재 날자의 시간은 00:00:00 으로 맞추기 위한 작업.
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		
+		System.out.println(c1.getTime());
+		System.out.println(c2.getTime());
+		
+		int totalDate = 0;
+		
+		// .after()   .before()
+		if(c1.after(c2)) {
+			while(c1.compareTo(c2) != 0) {
+				c1.add(Calendar.DATE, -1);
+				totalDate++;
+			}
+		} else {
+			while(c1.compareTo(c2) != 0) {
+				c1.add(Calendar.DATE, 1);
+				totalDate--;
+			}
+		}
+		
+		System.out.println("D-Day " + totalDate);
 	}
 	
 	public void ex03() {
@@ -42,7 +104,15 @@ public class Sample03 {
 		 * 동작 시킬 프로그램은 1 ~ 100,000 까지의 누적합을 구하는 코드로
 		 * 해당 반복이 얼만큼의 시간이 걸리는지 기록을 출력한다.
 		 */
+		Date start = new Date(); // 프로그램 시작 전에 시간 측정먼저
+		int tot = 0;
+		for(int i = 1; i <= 1000000000L; i++) {
+			tot += i;
+		}
+		Date end = new Date(); // 프로그램 종료 후 시간 측정
 		
+		long timer = end.getTime() - start.getTime();
+		System.out.println("걸린 시간 : " + timer / 1000.0 + " 초");
 	}
 	
 	public static void main(String[] args) {
